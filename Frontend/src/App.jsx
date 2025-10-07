b2aa508f3a6390286f3ef6c1a9166ce7cc3f962c
Anuj srivastav <anujsrivastav683@gmail.com>
2025-10-07 07:23:22 -0700

    adding routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
 import Main from "./components/home/Main";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import PropertyList from "./components/home/PropertyList" 
 
 function App() {
   return (
     <div className="App">
-      <Main/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />}>
+              <Route index element={<PropertyList />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- Frontend/src/components/home/Main.jsx
+++ Frontend/src/components/home/Main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from './Header'
-import PropertyList from './PropertyList'
+import {Outlet} from "react-router-dom"
+import Footer from './Footer'
+
 
 const Main = () => {
   return (
       <div>
       <Header />
-      <PropertyList/>
+      <Outlet />
+      <Footer/>
     </div>
   )
 }
