6ac27cb06e1b28400be4f22d029b0c4189252f96
Anuj srivastav <anujsrivastav683@gmail.com>
2025-10-11 23:04:30 -0700

    adding property-slice helps in state management

---
+++ Frontend/src/store/Property/property-action.js
@@ -0,0 +1,6 @@
+// import { propertyAction } from "./property-slice";
+// import { axiosInstance } from "../../utils/axios";
+
+// export const getAllProperties = () => async (dispatch, getState) => {
+    
+// }
