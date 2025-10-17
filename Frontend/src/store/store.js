9a425eea73042a15223b5ca7db1cf8b512575327
Anuj srivastav <anujsrivastav683@gmail.com>
2025-10-17 04:25:42 -0700

    adding property-details

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import propertySlice from "./Property/property-slice";
+import propertyDetailsSlice from "./PropertyDetails/propertyDetails-slice";
+
 
 const store = configureStore({
     reducer: {
-        properties: propertySlice.reducer
+        properties: propertySlice.reducer,
+        propertyDetails:propertyDetailsSlice.reducer
     }
 })
 
