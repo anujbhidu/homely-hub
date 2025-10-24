2512794be71d20e883e39a4a7f68dbf3463d89f6
Anuj srivastav <anujsrivastav683@gmail.com>
2025-10-23 22:15:05 -0700

    fix error and successfully fetch data

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,10 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    '/api': {
-      target: "http://localhost:8080",
-      changeOrigin: true,
-      secure: false
-    }
-  }
+    proxy: {
+      '/api': {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false
+      },
+    },
+  },
 })
